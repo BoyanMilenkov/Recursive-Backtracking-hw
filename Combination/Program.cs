4fe405cc029220ca7d10c6cb820283f574203254
BoyanMilenkov <80035234+BoyanMilenkov@users.noreply.github.com>
2022-02-11 10:27:59 +0200

    Add files via upload

---
+++ Combination/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Combination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] set = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int k = int.Parse(Console.ReadLine());
+
+            int[] array = new int[k];
+
+            Generate(array, 0, 0, set);
+        }
+        private static void Generate(int[] array, int index,  int start, int[] set)
+        {
+            if(index > array.Length - 1)
+            {
+                Console.WriteLine(string.Join(" ", array));
+                return;
+            }
+            for(int i = start; i < set.Length; i++)
+            {
+                array[index] = set[i];
+                Generate(array, index + 1, i + 1, set);
+            }
+        }
+    }
+}
---
+++ EightQueens/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EightQueens
+{
+    class Program
+    {
+        const int Size = 8;
+        static int[,] matrix = new int[Size, Size];
+        static HashSet<int> attackedRows = new HashSet<int>();
+        static HashSet<int> attackedCol = new HashSet<int>();
+        static HashSet<int> LeftDia = new HashSet<int>();
+        static void Main(string[] args)
+        {
+            PlaceQueens(0);
+        }
+        static void PlaceQueens(int row)
+        {
+            if (row == Size)
+            {
+                PrinstSolution();
+            }
+            for(int col = 0; col < Size; col++)
+            {
+                if(CanPlaceQueen(row, col))
+                {
+                    MarkAttackedPosition(row, col);
+                    PlaceQueens(row + 1);
+                    UnMarkAttackedPosition(row, col);
+                }
+            }
+        }
+
+        private static void UnMarkAttackedPosition(int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void MarkAttackedPosition(int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool CanPlaceQueen(int row, int col)
+        {
+            bool isFreePosition = attackedRows.Contains(row);
+        }
+
+        private static void PrinstSolution()
+        {
+            for(int r = 0; r < Size ; r++)
+            {
+                for(int c = 0; c < Size; c++)
+                {
+                    if(matrix[r, c] == 1)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
